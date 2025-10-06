9eec20a3e68e96b3fb12f41746aee55eec7d0d7e
JinJason123 <jinjason123@gmail.com>
2025-10-06 00:24:13 -0400

    scripts for aws

---
+++ app/Messages.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Messages = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Messages</Text>
+      
+
+      <Link href="/" style={styles.link}>Go to Home</Link>
+    </View>
+  )
+}
+
+export default Messages
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    header: {
+        height: 200,
+        backgroundColor: '#7DFCD5',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 45,
+        color: '#FFA850'
+    },
+    link: {
+        marginVertical: 20,
+        borderBottomWidth: 1,
+    }
+})
